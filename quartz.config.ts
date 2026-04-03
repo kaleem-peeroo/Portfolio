82cf5d4bb40f2207d0e5b94a99283b9b7ed2a8e9
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2026-04-03 19:55:17 +0100

    Site update: Fri  3 Apr 2026 19:55:17 BST

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,8 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "google",
+      tagId: "G-W45E6W22LM"
     },
     locale: "en-US",
     baseUrl: "kaleempeeroo.com",
